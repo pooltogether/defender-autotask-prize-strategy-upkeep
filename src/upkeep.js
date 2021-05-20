04b6b699f7b3539a01ee9cb2bd8cfeb8d1427291
Aodhgan <aodhgan@pooltogether.com>
2021-05-20 08:12:56 -0700

    returning early if pending transactions found

--- src/upkeep.js
+++ src/upkeep.js
@@ -15,7 +15,27 @@ exports.upkeep = async function (relayer, network) {
   console.log("checkUpkeep() result ", upkeepNeeded)
 
   if (upkeepNeeded) {
-    console.log("performing upkeep on ", prizeStrategyUpkeep.address)
+    // check if the same tranasction still in flight
+    const txs = await relayer.list({
+      since: new Date(Date.now() - 3600 * 1000),// 
+      status: 'pending', // can be 'pending', 'mined', or 'failed'
+      limit: 5, // newest txs will be returned first
+    })
+
+    // if non-empty array then return
+    // this way only gas-speedup transactions will happen and not duplicate transactions
+    if (array.length > 0 || array) {
+      console.log("The following transactions are in flight: ")
+      txs.forEach(tx => {
+        console.log(tx)
+      });
+
+      console.log("Transactions already in flight - returning early")
+      return
+    }
+
+
+    console.log("calling peformUpkeep() on ", prizeStrategyUpkeep.address)
     const unsignedTx = await prizeStrategyUpkeep.populateTransaction.performUpkeep([])    
     const gasLimit = ((await prizeStrategyUpkeep.estimateGas.performUpkeep([])).toNumber() * 2) 
     console.log(`performUpkeep(). Gas limit: ${gasLimit.toString()}`)
@@ -25,5 +45,6 @@ exports.upkeep = async function (relayer, network) {
       gasLimit,
       speed: 'fast'
     })
+
   }
 }
